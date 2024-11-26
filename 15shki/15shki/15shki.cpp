dcafe216e5e1ee927caaf2c8818fc7bbad6f9d23
Fedor-erohin-IT14 <erohonf@gmail.com>
2024-11-26 10:59:27 +0500

    проверка условий победы

--- 15shki/15shki/15shki.cpp
+++ 15shki/15shki/15shki.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 void RandZap(int** mas)
 {
 
@@ -8,5 +5,20 @@ void RandZap(int** mas)
 
 
 
-
-
+bool ProvPob(int** mas)
+{
+  int r=1;
+    for (int i=0;i<16;i++)
+        {
+            if (mas[i]==i+1)
+            {
+              r=r*1;
+            }
+          else
+            {
+              r=r*0;
+            }
+          
+        }
+  return r;
+}
