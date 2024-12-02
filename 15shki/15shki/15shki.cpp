2ea324db30b039f6089fe0cd8b30abd1a99c9507
Fedor-erohin-IT14 <erohonf@gmail.com>
2024-12-03 00:57:33 +0500

    случайное заполнение

--- 15shki/15shki/15shki.cpp
+++ 15shki/15shki/15shki.cpp
@@ -1,7 +1,35 @@
-void RandZap(int** mas)
-{
+#include <algorithm>
 
+void RandZap(int** mas, int n, int m)
+{
+	int t = 0;
+	int a[16] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+	random_shuffle(&a[0], &a[16]);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+
+			 mas[i][j]=a[t];
+			 t += 1;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+			if (mas[i][j]<10)
+			{
+				cout <<" " << mas[i][j] << " ";
+			}
+			else
+			{
+				cout << mas[i][j] << " ";
+			}
+			
+		cout << "\n";
+	}
 }
+
 
 
 
