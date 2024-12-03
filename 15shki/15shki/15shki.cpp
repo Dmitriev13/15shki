9b07aaf78ac3bf2e8002f7637176099e6c07bd89
Dmitriev13 <vanyadmitriev483@gmail.com>
2024-12-03 10:15:24 +0500

    Update 15shki.cpp

--- 15shki/15shki/15shki.cpp
+++ 15shki/15shki/15shki.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "header.h"
+
 using namespace std;
+
 int main()
 {
     setlocale (LC_ALL,"rus");
     cout << "Игра - пятняшки"<<endl;
     cout << "Цель игры - на поле 4х4 следует разместить цифры в порядке возрастания от 1 до 15, 0 - пустая клетка"<<endl;
+    cout << "Удачи!\n";
+    
 }
