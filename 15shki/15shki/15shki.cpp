07e51a835ddb2f761dc01a0bb87d99417b53e45a
Fedor-erohin-IT14 <erohonf@gmail.com>
2024-12-03 10:11:39 +0500

    Update m,n=>4

--- 15shki/15shki/15shki.cpp
+++ 15shki/15shki/15shki.cpp
@@ -1,22 +1,22 @@
 #include <algorithm>
 
-void RandZap(int** mas, int n, int m)
+void RandZap(int** mas)
 {
 	int t = 0;
 	int a[16] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
 	random_shuffle(&a[0], &a[16]);
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < 4; j++)
 		{
 
 			 mas[i][j]=a[t];
 			 t += 1;
 		}
 	}
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < 4; j++)
 			if (mas[i][j]<10)
 			{
 				cout <<" " << mas[i][j] << " ";
@@ -35,28 +35,27 @@ void RandZap(int** mas, int n, int m)
 
 bool ProvPob(int** mas)
 {
-int t=0;
-int r=1;
-for (int i = 0; i < n; i++)
-{
-	for (int j = 0; j < m; j++)
+	int t = 0;
+	int r = 1;
+	for (int i = 0; i < 4; i++)
 	{
+		for (int j = 0; j < 4; j++)
+		{
 
-		if( mas[i][j]==t)
-		  {
-              		r=r*1;
-			t+=1;
-           	  }
-          	else
-           	  {
-              		r=r*0;
-            	  }
-	
+			if (mas[i][j] == t)
+			{
+				r = r * 1;
+				t += 1;
+			}
+			else
+			{
+				r = r * 0;
+			}
+
+		}
+		return r;
 	}
-	return r;
-}	
 }
-
 
 
 
