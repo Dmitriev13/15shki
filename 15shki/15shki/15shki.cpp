f68401d461ab0b86edbb1c777750d86012cc7692
Fedor-erohin-IT14 <erohonf@gmail.com>
2024-12-03 10:02:14 +0500

    Update проверки

--- 15shki/15shki/15shki.cpp
+++ 15shki/15shki/15shki.cpp
@@ -58,19 +58,5 @@ for (int i = 0; i < n; i++)
 
 
 
-	
-  int r=1;
-    for (int i=0;i<16;i++)
-        {
-            if (mas[i]==i+1)
-            {
-              r=r*1;
-            }
-          else
-            {
-              r=r*0;
-            }
-          
-        }
-  return r;
-}
+
+
