eb220be7e7fcd9c26f0be74b9e396fafe21ab96f
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2024-12-09 18:46:05 +0500

    Main без заголовочного файла

--- 15shki/15shki/15shechki.cpp
+++ 15shki/15shki/15shechki.cpp
@@ -5,10 +5,46 @@ using namespace std;
 
 int main()
 {
-    setlocale (LC_ALL,"rus");
-    cout << "Игра - пятняшки"<<endl;
-    cout << "Цель игры - на поле 4х4 следует разместить цифры в порядке возрастания от 1 до 15, 0 - пустая клетка"<<endl;
-    cout << "Удачи!\n";
-    void RandZap(int** mas);
-    int move(int x, int** arr);
+   main() {
+    setlocale(LC_ALL, "ru");
+    srand(time(0));
+
+    cout << "Игра - пятнашки" << endl;
+    cout << "Цель игры - на поле 4х4 следует разместить цифры в порядке возрастания от 1 до 15, 0 - пустая клетка" << endl;
+    cout << "Удачи!" << endl;
+
+    int** mas = new int* [4];
+    for (int i = 0; i < 4; i++)
+    {
+        mas[i] = new int[4];
+    }
+
+    RandZap(mas);
+    while (true) {
+        if (ProvProb(mas))
+        {
+            cout << "Победа!" << endl;
+            break;
+        }
+
+        cout << "Введите число, которое хотите переместить: ";
+        int x;
+        cin >> x;
+
+        move(x, mas);
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (mas[i][j] < 10) {
+                    cout << " " << mas[i][j] << " ";
+                }
+                else {
+                    cout << mas[i][j] << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+}
+
 }
