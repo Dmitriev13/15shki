72bc269344a9ac3b601c5d2310ccceecc0c22c96
Dmitriev13 <vanyadmitriev483@gmail.com>
2024-12-10 10:30:13 +0500

    Update 1.cpp

--- 15shki/15shki/1.cpp
+++ 15shki/15shki/1.cpp
@@ -1,7 +1,8 @@
 #include "header.h"
 
 void move(int x, int** mas)
-{
+{    
+    if (x >= 0 and x <=15){
     int xi = 0, yi = 0, x0 = 0, y0 = 0;
     for (int i = 0; i < 4; i++)
     {
@@ -18,6 +19,11 @@ void move(int x, int** mas)
                 y0 = j;
             }
         }
+    }
+    }
+    else {
+        cout << "Ваше значение не входит в диапозон!";
+        continue;
     }
 
     if ((abs(xi - x0) + abs(yi - y0)) == 1)
