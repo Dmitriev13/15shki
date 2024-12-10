291cdca07c24a2e379918102c3aaef5c6497076d
Dmitriev13 <vanyadmitriev483@gmail.com>
2024-12-10 10:41:00 +0500

    Update 2.cpp

--- 15shki/15shki/2.cpp
+++ 15shki/15shki/2.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <algorithm>
 #include "header.h"
+#include <random>
 
 using namespace std;
 
-void RandZap(int** mas)
+void Randzap(int** mas)
 {
 	int t = 0;
 	int a[16] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
@@ -59,6 +60,7 @@ bool ProvProb(int** mas)
 		}
 		return r;
 	}
+return true;
 }
 
 
