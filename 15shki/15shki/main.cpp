7cf727bc85cee15bd8e21e074408e727335e52bd
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2024-12-15 20:59:30 +0500

    Исправил

--- 15shki/15shki/main.cpp
+++ 15shki/15shki/main.cpp
@@ -3,15 +3,13 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "ru");
     srand(time(0));
 
     cout << "Игра - пятнашки" << endl;
     cout << "Цель игры - на поле 4х4 следует разместить цифры в порядке возрастания от 1 до 15, 0 - пустая клетка" << endl;
     cout << "Удачи!" << endl;
-    int x;
 
     int** mas = new int* [4];
     for (int i = 0; i < 4; i++)
@@ -19,7 +17,7 @@ int main()
         mas[i] = new int[4];
     }
 
-    Randzap(mas);
+    RandZap(mas);
     while (true) {
         if (ProvProb(mas))
         {
@@ -28,28 +26,24 @@ int main()
         }
 
         cout << "Введи число, которое хотите переместить: ";
-        while (!(cin >> x)) 
-        {
+        int x;
+        while (!(cin >> x)) {
             cin.clear();
             cin.ignore(1, '\n');
-            for (int i = 0; i < 4; i++) 
-            {
-                for (int j = 0; j < 4; j++) 
-                {
-                    if (mas[i][j] < 10) 
-                    {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (mas[i][j] < 10) {
                         cout << " " << mas[i][j] << " ";
                     }
-                        else 
-                        {
-                            cout << mas[i][j] << " ";
-                        }
+                    else {
+                        cout << mas[i][j] << " ";
+                    }
                 }
                 cout << endl;
             }
             cout << "Ошибка, введи число: ";
         }
-    }
+
         move(x, mas);
 
         for (int i = 0; i < 4; i++) {
